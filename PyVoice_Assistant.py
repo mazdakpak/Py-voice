b48631c9621555cd8f9438b3d0b147fcd805b772
Mazdak Pakaghideh <babamazdak@gmail.com>
2021-01-10 22:55:35 +0330

    Add speech Recognizer function

--- PyVoice_Assistant.py
+++ PyVoice_Assistant.py
@@ -3,7 +3,11 @@ import time
 import speech_recognition as sr
 from gtts import gTTS
 import playsound
+import sounddevice
+import pyaudio
 
+
+#a function to  speak a text
 def speak(text):
     tts = gTTS(text=text ,lang="en")
     file_name =  "voice.mp3"
@@ -11,3 +15,16 @@ def speak(text):
     playsound.playsound(file_name)
     os.remove(file_name)
 
+
+def get_audio():
+    r = sr.Recognizer()
+    with  sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as ex:
+            print("Error :"+str(ex))
+    return said
+get_audio()
